2137089e85fb8e2714037f19d6fc2153f87f1579
danyuhao <danyuhao@users.noreply.github.com>
2016-02-22 12:40:09 +0800

    Added files via upload

---
+++ .emacs.d/init.el
@@ -0,0 +1,251 @@
+(setq url-proxy-services
+   '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+     ("http" . "proxy.xxxx.com:8080")
+     ("https" . "proxy.xxxx.com:8080")))
+
+(setq url-http-proxy-basic-auth-storage
+    (list (list "proxy.xxxx.com:8080"
+                (cons "Input your LDAP UID !"
+                      (base64-encode-string "username:password")))))
+
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+(setq gc-cons-threshold 100000000)
+(setq inhibit-startup-message t)
+
+;; Setting English Font
+(set-face-attribute
+ 'default nil :font "Consolas 12")
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(defconst demo-packages
+  '(anzu
+    company
+    duplicate-thing
+    ggtags
+    helm
+    helm-gtags
+    helm-projectile
+    helm-swoop
+    ;; function-args
+    clean-aindent-mode
+    comment-dwim-2
+    dtrt-indent
+    ws-butler
+    iedit
+    yasnippet
+    smartparens
+    projectile
+    volatile-highlights
+    undo-tree
+    zygospore))
+
+(defun install-packages ()
+  "Install all required packages."
+  (interactive)
+  (unless package-archive-contents
+    (package-refresh-contents))
+  (dolist (package demo-packages)
+    (unless (package-installed-p package)
+      (package-install package))))
+
+(install-packages)
+
+;; this variables must be set before load helm-gtags
+;; you can change to any prefix key of your choice
+(setq helm-gtags-prefix-key "\C-cg")
+
+(add-to-list 'load-path "~/.emacs.d/custom")
+
+(require 'setup-helm)
+(require 'setup-helm-gtags)
+;; (require 'setup-ggtags)
+(require 'setup-cedet)
+(require 'setup-editing)
+
+(windmove-default-keybindings)
+
+;; function-args
+;; (require 'function-args)
+;; (fa-config-default)
+;; (define-key c-mode-map  [(tab)] 'company-complete)
+;; (define-key c++-mode-map  [(tab)] 'company-complete)
+
+;; company
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+(delete 'company-semantic company-backends)
+(define-key c-mode-map  [(tab)] 'company-complete)
+(define-key c++-mode-map  [(tab)] 'company-complete)
+;; (define-key c-mode-map  [(control tab)] 'company-complete)
+;; (define-key c++-mode-map  [(control tab)] 'company-complete)
+
+;; company-c-headers
+(add-to-list 'company-backends 'company-c-headers)
+
+;; hs-minor-mode for folding source code
+(add-hook 'c-mode-common-hook 'hs-minor-mode)
+
+;; Available C style:
+;; “gnu”: The default style for GNU projects
+;; “k&r”: What Kernighan and Ritchie, the authors of C used in their book
+;; “bsd”: What BSD developers use, aka “Allman style” after Eric Allman.
+;; “whitesmith”: Popularized by the examples that came with Whitesmiths C, an early commercial C compiler.
+;; “stroustrup”: What Stroustrup, the author of C++ used in his book
+;; “ellemtel”: Popular C++ coding standards as defined by “Programming in C++, Rules and Recommendations,” Erik Nyquist and Mats Henricson, Ellemtel
+;; “linux”: What the Linux developers use for kernel development
+;; “python”: What Python developers use for extension modules
+;; “java”: The default style for java-mode (see below)
+;; “user”: When you want to define your own style
+(setq
+ c-default-style "linux" ;; set style to "linux"
+ )
+
+(global-set-key (kbd "RET") 'newline-and-indent)  ; automatically indent when press RET
+
+;; activate whitespace-mode to view all whitespace characters
+(global-set-key (kbd "C-c w") 'whitespace-mode)
+
+;; show unncessary whitespace that can mess up your diff
+(add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
+
+;; use space to indent by default
+(setq-default indent-tabs-mode nil)
+
+;; set appearance of a tab that is represented by 4 spaces
+(setq-default tab-width 4)
+
+;; Compilation
+(global-set-key (kbd "<f5>") (lambda ()
+                               (interactive)
+                               (setq-local compilation-read-command nil)
+                               (call-interactively 'compile)))
+
+;; setup GDB
+(setq
+ ;; use gdb-many-windows by default
+ gdb-many-windows t
+
+ ;; Non-nil means display source file containing the main routine at startup
+ gdb-show-main t
+ )
+
+;; Package: clean-aindent-mode
+(require 'clean-aindent-mode)
+(add-hook 'prog-mode-hook 'clean-aindent-mode)
+
+;; Package: dtrt-indent
+(require 'dtrt-indent)
+(dtrt-indent-mode 1)
+
+;; Package: ws-butler
+(require 'ws-butler)
+(add-hook 'prog-mode-hook 'ws-butler-mode)
+
+;; Package: yasnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; Package: smartparens
+(require 'smartparens-config)
+(setq sp-base-key-bindings 'paredit)
+(setq sp-autoskip-closing-pair 'always)
+(setq sp-hybrid-kill-entire-symbol nil)
+(sp-use-paredit-bindings)
+
+(show-smartparens-global-mode +1)
+(smartparens-global-mode 1)
+
+;; Package: projejctile
+(require 'projectile)
+(projectile-global-mode)
+(setq projectile-enable-caching t)
+
+(require 'helm-projectile)
+(helm-projectile-on)
+(setq projectile-completion-system 'helm)
+(setq projectile-indexing-method 'alien)
+
+;; Package zygospore
+(global-set-key (kbd "C-x 1") 'zygospore-toggle-delete-other-windows)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   (quote
+    ((company-clang-arguments "-I/usr1/yuhao/cmc/src/" "-I/usr1/yuhao/cmc/src/include/")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(if (eq system-uses-terminfo t)
+    (progn                              ;; PuTTY hack - needs to be in SCO mode
+      (define-key key-translation-map [\e] [\M])
+      (define-key input-decode-map "\e[H" [home])
+      (define-key input-decode-map "\e[F" [end])
+      (define-key input-decode-map "\e[D" [S-left])
+      (define-key input-decode-map "\e[C" [S-right])
+      (define-key input-decode-map "\e[A" [S-up])
+      (define-key input-decode-map "\e[B" [S-down])
+      (define-key input-decode-map "\e[C" [S-right])
+      (define-key input-decode-map "\e[I" [prior])
+      (define-key input-decode-map "\e[G" [next])
+      (define-key input-decode-map "\e[M" [f1])
+      (define-key input-decode-map "\e[Y" [S-f1])
+      (define-key input-decode-map "\e[k" [C-f1])
+      (define-key input-decode-map "\e\e[M" [M-f1])
+      (define-key input-decode-map "\e[N" [f2])
+      (define-key input-decode-map "\e[Z" [S-f2])
+      (define-key input-decode-map "\e[l" [C-f2])
+      (define-key input-decode-map "\e\e[N" [M-f2])
+      (define-key input-decode-map "\e[O" [f3])
+      (define-key input-decode-map "\e[a" [S-f3])
+      (define-key input-decode-map "\e[m" [C-f3])
+      (define-key input-decode-map "\e\e[O" [M-f3])
+      (define-key input-decode-map "\e[P" [f4])
+      (define-key input-decode-map "\e[b" [S-f4])
+      (define-key input-decode-map "\e[n" [C-f4])
+      (define-key input-decode-map "\e\e[P" [M-f4])
+      (define-key input-decode-map "\e[Q" [f5])
+      (define-key input-decode-map "\e[c" [S-f5])
+      (define-key input-decode-map "\e[o" [C-f5])
+      (define-key input-decode-map "\e\e[Q" [M-f5])
+      (define-key input-decode-map "\e[R" [f6])
+      (define-key input-decode-map "\e[d" [S-f6])
+      (define-key input-decode-map "\e[p" [C-f6])
+      (define-key input-decode-map "\e\e[R" [M-f6])
+      (define-key input-decode-map "\e[S" [f7])
+      (define-key input-decode-map "\e[e" [S-f7])
+      (define-key input-decode-map "\e[q" [C-f7])
+      (define-key input-decode-map "\e\e[S" [M-f7])
+      (define-key input-decode-map "\e[T" [f8])
+      (define-key input-decode-map "\e[f" [S-f8])
+      (define-key input-decode-map "\e[r" [C-f8])
+      (define-key input-decode-map "\e\e[T" [M-f8])
+      (define-key input-decode-map "\e[U" [f9])
+      (define-key input-decode-map "\e[g" [S-f9])
+      (define-key input-decode-map "\e[s" [C-f9])
+      (define-key input-decode-map "\e\e[U" [M-f9])
+      (define-key input-decode-map "\e[V" [f10])
+      (define-key input-decode-map "\e[h" [S-f10])
+      (define-key input-decode-map "\e[_" [C-f10])
+      (define-key input-decode-map "\e\e[V" [M-f10])
+      (define-key input-decode-map "\e[W" [f11])
+      (define-key input-decode-map "\e[i" [S-f11])
+      (define-key input-decode-map "\e[u" [C-f11])
+      (define-key input-decode-map "\e\e[W" [M-f11])
+      (define-key input-decode-map "\e[X" [f12])
+      (define-key input-decode-map "\e[j" [S-f12])
+      (define-key input-decode-map "\e[v" [C-f12])
+      (define-key input-decode-map "\e\e[X" [M-f12])))
+
+(require 'color-theme-solarized)
+(color-theme-solarized-light)
